5671bc7f8677c2c8aebe5c4824f862372f3cdc7f
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-06-30 23:55:28 +0300

    Added some real users and tweets

--- src/Twitter.Service/ServiceHost.cs
+++ src/Twitter.Service/ServiceHost.cs
@@ -1,5 +1,8 @@
-﻿using System;
-using Akka.Actor;
+﻿using Akka.Actor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Topshelf;
@@ -23,25 +26,26 @@ namespace Twitter.Service
 
         private void EmulateConnectionsAsync()
         {
-            var users = new[] {"John", "Ivan", "Ann", "Alex"};
+            var users = JsonConvert.DeserializeObject<List<UserData>>(File.ReadAllText("userdata.json"));
+
             var rand = new Random();
             Task.Delay(10000)
                 .ContinueWith(_ =>
                 {
-                    foreach (var username in users)
+                    foreach (var user in users)
                     {
                         Thread.Sleep(250);
                         SystemActors.ApiActor.Tell(
-                            new StartNewSessionCommand(new UserSession(username, ActorRefs.Nobody)));
+                            new StartNewSessionCommand(new UserSession(user.Username, ActorRefs.Nobody)));
                     }
                 })
                 .ContinueWith(_ =>
                 {
                     for (var i = 0; i < 1000; ++i)
                     {
-                        Thread.Sleep(rand.Next(500, 1000));
-                        var userIndex = rand.Next(0, users.Length);
-                        SystemActors.ApiActor.Tell(new NewMessagePosted(users[userIndex], "Test"));
+                        Thread.Sleep(rand.Next(200, 1000));
+                        var userIndex = rand.Next(0, users.Count);
+                        SystemActors.ApiActor.Tell(new NewMessagePosted(users[userIndex].Username, users[userIndex].Messages[i % users[userIndex].Messages.Count]));
                     }
                     SystemActors.ApiActor.Tell(new NewMessagePosted("Administrator", "The END"));
                 });
@@ -51,6 +55,15 @@ namespace Twitter.Service
         {
             ActorSystem.Shutdown();
             return true;
+        }
+
+        public class UserData
+        {
+            [JsonProperty(PropertyName = "username")]
+            public string Username { get; set; }
+
+            [JsonProperty(PropertyName = "messages")]
+            public List<string> Messages { get; set; }
         }
     }
 }
