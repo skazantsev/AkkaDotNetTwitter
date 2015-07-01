f212042399ea2e9b339ce3730785c2fbd613a026
Sergey Kazantsev <sergey.kazantsev1@gmail.com>
2015-07-02 00:18:20 +0300

    Separated users emulator

---
+++ src/Twitter.Service/Components/TwitterFeedEmulator.cs
@@ -0,0 +1,60 @@
+﻿using Akka.Actor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Twitter.Service.Actors;
+using Twitter.Shared.Business;
+using Twitter.Shared.Messages;
+
+namespace Twitter.Service.Components
+{
+    public class TwitterFeedEmulator
+    {
+        private List<UserData> _users;
+ 
+        public void EmulateUserActivity()
+        {
+            ReadUsers();
+            Task.Delay(8000)
+                .ContinueWith(_ => ConnectUsers())
+                .ContinueWith(_ => StartTweeting());
+        }
+
+        private void ReadUsers()
+        {
+            _users = JsonConvert.DeserializeObject<List<UserData>>(File.ReadAllText("userdata.json"));
+        }
+
+        private void ConnectUsers()
+        {
+            foreach (var user in _users)
+            {
+                Thread.Sleep(250);
+                SystemActors.ApiActor.Tell(
+                    new StartNewSessionCommand(new UserSession(user.Username, ActorRefs.Nobody)));
+            }
+        }
+
+        private void StartTweeting()
+        {
+            var rand = new Random();
+            for (var i = 0; i < 1000; ++i)
+            {
+                Thread.Sleep(rand.Next(1000, 2000));
+                var user = _users[rand.Next(0, _users.Count)];
+                SystemActors.ApiActor.Tell(new NewMessagePosted(user.Username, user.Messages[i % user.Messages.Count]));
+            }
+            SystemActors.ApiActor.Tell(new NewMessagePosted("UFO", "The emulator has finished its work"));
+        }
+
+        protected class UserData
+        {
+            public string Username { get; set; }
+
+            public List<string> Messages { get; set; }
+        }
+    }
+}
--- src/Twitter.Service/ServiceHost.cs
+++ src/Twitter.Service/ServiceHost.cs
@@ -1,14 +1,7 @@
 ﻿using Akka.Actor;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using Topshelf;
 using Twitter.Service.Actors;
-using Twitter.Shared.Business;
-using Twitter.Shared.Messages;
+using Twitter.Service.Components;
 
 namespace Twitter.Service
 {
@@ -20,50 +13,14 @@ namespace Twitter.Service
         {
             ActorSystem = ActorSystem.Create("twitterservice");
             SystemActors.ApiActor = ActorSystem.ActorOf(Props.Create(() => new ApiActor()), "api");
-            EmulateConnectionsAsync();
+            new TwitterFeedEmulator().EmulateUserActivity();
             return true;
-        }
-
-        private void EmulateConnectionsAsync()
-        {
-            var users = JsonConvert.DeserializeObject<List<UserData>>(File.ReadAllText("userdata.json"));
-
-            var rand = new Random();
-            Task.Delay(10000)
-                .ContinueWith(_ =>
-                {
-                    foreach (var user in users)
-                    {
-                        Thread.Sleep(250);
-                        SystemActors.ApiActor.Tell(
-                            new StartNewSessionCommand(new UserSession(user.Username, ActorRefs.Nobody)));
-                    }
-                })
-                .ContinueWith(_ =>
-                {
-                    for (var i = 0; i < 1000; ++i)
-                    {
-                        Thread.Sleep(rand.Next(1000, 2000));
-                        var userIndex = rand.Next(0, users.Count);
-                        SystemActors.ApiActor.Tell(new NewMessagePosted(users[userIndex].Username, users[userIndex].Messages[i % users[userIndex].Messages.Count]));
-                    }
-                    SystemActors.ApiActor.Tell(new NewMessagePosted("Administrator", "The END"));
-                });
         }
 
         public bool Stop(HostControl hostControl)
         {
             ActorSystem.Shutdown();
             return true;
-        }
-
-        public class UserData
-        {
-            [JsonProperty(PropertyName = "username")]
-            public string Username { get; set; }
-
-            [JsonProperty(PropertyName = "messages")]
-            public List<string> Messages { get; set; }
         }
     }
 }
